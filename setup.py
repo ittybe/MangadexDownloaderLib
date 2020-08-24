d700482b6b49557844d6a27281dfe379a6287d11
ittybe <misha.kan7.2004.20@gmail.com>
2020-08-24 20:49:11 +0300

    change structure of project

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-from pip.req import parse_requirements
-
+import setuptools
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt")
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
 reqs = [str(ir.req) for ir in install_reqs]
 
-setup(
-    ...
+setuptools.setup(
     install_requires=reqs
 )
